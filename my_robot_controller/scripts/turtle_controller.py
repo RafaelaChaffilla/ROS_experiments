ffbcdcf8e3c29855982e74fd8c36cdbdb400b461
RafaelaChaffilla <rafaela.chaffilla@tecnico.ulisboa.pt>
2023-11-20 00:00:10 +0100

    heart changing colors

--- my_robot_controller/scripts/turtle_controller.py
+++ my_robot_controller/scripts/turtle_controller.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import rospy
-from turtlesim.msg import Pose
-from geometry_msgs.msg import Twist
+from turtlesim.msg      import Pose
+from geometry_msgs.msg  import Twist
+from turtlesim.srv      import SetPen
 import numpy as np
 
 init_x, init_y = None, None
@@ -12,31 +13,44 @@ state = 'init'
 # define when circle will stop being drawn
 y_margin = 0.7
 
+def call_set_pen(r,g,b,width, off):
+    ''' All input arguments have to be uint8'''
+    try:
+        set_pen = rospy.ServiceProxy('/turtle1/set_pen', SetPen)
+        response = set_pen(r, g, b, width, off)
+        # this service doesn't give a response
+        # rospy.loginfo(response)
+    except rospy.ServiceException as err:
+        rospy.logwarn(err)
+
 def set_state(pose: Pose):
     '''
         returns the current state that the path should take
     '''
     if state == 'init' and pose.theta >= np.pi/2:
         rospy.loginfo('switching states: ' + state)
+        call_set_pen(135, 35, 65, 4, 0)
         return 'circle'
     elif state == 'circle' and pose.y <= init_y - y_margin:
         rospy.loginfo('switching states: ' + state)
-        rospy.loginfo('pose.y: ' + str(pose.y))
-        rospy.loginfo('init.y: ' + str(init_y))
+        call_set_pen(240, 89, 65, 4, 0)
         return 'down_line'
     elif state == 'down_line' and pose.x >= init_x:
         rospy.loginfo('switching states: ' + state)
         return 'tip'
     elif state == 'tip' and pose.theta >= -init_theta:
         rospy.loginfo('switching states: ' + state)
+        call_set_pen(190, 49, 68, 4, 0)
         return 'up_line'
     elif state == 'up_line' and pose.y >= init_y - y_margin:
         rospy.loginfo('switching states: ' + state)
+        call_set_pen(34, 9, 44, 4, 0)
         return 'circle'
     elif (state == 'circle'
           and pose.x >= init_x - 0.1 and pose.x <= init_x + 0.1
           and pose.y >= init_y - 0.1 and pose.y <= init_y + 0.1
           and pose.theta < 0):
+        call_set_pen(34, 9, 44, 0, 3)
         return 'stop'
     else:
         return state
@@ -71,7 +85,13 @@ def tip(pose: Pose):
     return rule
 
 def stop(pose: Pose):
-    rule = Twist()
+    if(pose.y <= init_y - 0.5):
+        rospy.signal_shutdown('Hertz complete <3')
+        # return
+    else:
+        rule = Twist()
+        rule.linear.x = 1
+        rule.angular.z = 0.2
     return rule
 
 state_functions = {
@@ -91,6 +111,9 @@ def pose_callback(msg: Pose):
 
 if __name__ == '__main__':
     rospy.init_node('turtle_controller')
+
+    # block the code from happening while the service is not up
+    rospy.wait_for_service("/turtle1/set_pen")
 
     # create pub before sub ? prolly so that the sub knows the pub to publish inside the callback
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
